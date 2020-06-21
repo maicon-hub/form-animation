1c0c8d661b44b4f939bd8a70438bf36787e59157
maicon <maiconandradexd@gmail.com>
2020-06-20 23:31:39 -0300

    background animation created

--- src/script.js
+++ src/script.js
@@ -33,3 +33,31 @@ form.addEventListener("animationend", (event) => {
     document.querySelector("body").style.overflow = "hidden";
   };
 });
+
+const ulSquares = document.querySelector("ul.squares");
+
+for (let i = 0; i < 11; i++) {
+  const li = document.createElement("li");
+
+  const random = (max, min) => Math.random() * (max - min) + min
+
+  const size = Math.floor(random(120, 10));
+  const position = random(99, 1);
+  const delay = random(5, 0.1);
+
+  const duration = random(12, 24);
+
+  li.style.width = `${size}px`;
+  li.style.height = `${size}px`;
+  li.style.bottom = `-${size}px`;
+
+  li.style.left = `${position}%`;
+
+  li.style.animationDelay = `${delay}s`;
+
+  li.style.animationDuration = `${duration}s`
+
+  li.style.animationTimingFunction = `cubic-bezier(${Math.random}, ${Math.random}, ${Math.random}, ${Math.random})`
+
+  ulSquares.appendChild(li);
+}
